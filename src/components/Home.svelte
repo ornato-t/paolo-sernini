c147142fb512cede8726ae4aa4a9c74505e1d208
ornato_t <tommy.ornato@gmail.com>
2023-05-13 23:31:14 +0200

    Adding two more possible designs for home page

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -82,3 +82,26 @@
       </div>
     </div>
 </div>
+
+<div class="hero bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img src="/logo.webp" class="max-w-sm rounded-lg shadow-2xl" alt="logo"/>
+      <div>
+        <h1 class="text-5xl font-bold">{name}</h1>
+        <p class="py-6">{description}</p>
+        <button class="btn btn-primary">Scopri</button>
+      </div>
+    </div>
+</div>
+
+<div class="hero bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img src="/logo.webp" class="max-w-sm rounded-lg shadow-2xl" alt="logo"/>
+      <div>
+        <h1 class="text-5xl font-bold">{name}</h1>
+        <p class="py-6">{quote}</p>
+        <button class="btn btn-primary">Scopri</button>
+      </div>
+    </div>
+</div>
+
