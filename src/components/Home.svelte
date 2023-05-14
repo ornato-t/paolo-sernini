989fe5e81b709c59deb3c9090e284f925d2ebfcc
ornato_t <tommy.ornato@gmail.com>
2023-05-14 18:38:47 +0200

    Adding anchor links to home page and contacts (footer) in navbar

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,6 +3,10 @@
     const quote = '"La risoluzione di un problema parte sempre dalla sua comprensione"';
     const description = "Un professionista che, attraverso competenze trasversali alla medicina tradizionale, si colloca nei percorsi terapeutici con il compito di delucidare e potenziare l'efficacia psifofisica del percorso stesso.";
 </script>
+
+<!-- TODO: add this to the chosen component -->
+<span id="home"/>
+
 <div class="hero">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
