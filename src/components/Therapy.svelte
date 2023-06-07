ff72ebdc784cb44789ea0f86f033da5243a10cd3
Tommaso Ornato <tommy.ornato@gmail.com>
2023-06-07 10:24:30 +0200

    Updating therapy and bio pages according to Paolo's insights

--- src/components/Therapy.svelte
+++ src/components/Therapy.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-    const heading1 = 'Di che cosa si occupa un';
-    const heading2 = 'facilitatore terapeutico';
-    const heading3 = '?';
-    const points = ['Ah non lo so io', 'Suppongo faccia qualcosa', 'Spero qualcosa di utile'];  //TODO
-    const explanation = 'Il facilitatore terapeutico è una figura pazza che fa cose altrettanto pazze. Ho bisogno di inserire testo a caso. Lorem ipsum dolor sit amet.'
+  const points = [
+    {
+      bold: 'Migliorare la comprensione dei percorsi terapeutici intrapresi',
+      normal: 'attraverso la cura dell’aspetto psicofisico.',
+    }, {
+      bold: 'Promuovere un corretto stile di vita',
+      normal: 'andando a lavorare sulla prevenzione delle principali problematiche matrici di disequilibri patologici.',
+    }, {
+      bold: 'Istruire e accompagnare nella scoperta del proprio stato di stress',
+      normal: 'cercando di catturare e insegnare a catturare quelle che sono le potenzialità di questo stato ancestrale di adattamento, comprendendolo e non combattendolo strenuamente.',
+    }, {
+      bold: 'Offrire percorsi terapeutici complementari',
+      normal: 'basandosi sull’analisi della patologia cronica non come un semplice problema chimico, ma come un più complesso intreccio di disequilibri psico-fisiologici.',
+    }, {        
+      bold: 'Creare una connessione e una vicinanza con il soggetto',
+      normal: 'al fine di poter analizzare in profondità quelle che sono le radici di eventuali cronicizzazioni delle patologie.',
+    }
+  ] satisfies point[];
+
+  interface point {
+    bold: string,
+    normal: string,
+  }
 </script>
 
 <span id="facilitatore"/>
@@ -11,14 +29,23 @@
 <div class="hero min-h-screen bg-[url(/therapy.webp)]">
     <div class="hero-overlay bg-opacity-70"></div>
     <div class="hero-content text-start text-neutral-content shadow-lg">
-      <div class="max-w-xl">
-        <h1 class="mb-5 text-5xl font-bold text-center">{heading1} <span class="text-secondary">{heading2}</span>{heading3}</h1>
-        <ul class="mb-5 ml-12 list-disc text-lg">
+      <div class="max-w-full lg:max-w-4xl">
+        <h1 class="mb-2 px-4 lg:px-0 text-3xl lg:text-5xl font-bold text-center"> 
+          Di che cosa si occupa un 
+          <span class="text-secondary">facilitatore terapeutico</span>
+          ?
+        </h1>
+        <ul class="mb-5 ml-6 list-disc text-sm lg:text-base">
             {#each points as point}
-                <li>{point}</li>
+                <li><span class="font-semibold">{point.bold}</span> <span>{point.normal}</span></li>
             {/each}
         </ul>
-        <p class="mb-5">{explanation}</p>
+        <p class="mb-5">
+            Il Facilitatore Terapeutico, quindi, va ad assolvere a quello che è il ruolo del 
+            <span class="italic">"maestro"</span>, istruendo, correggendo e accompagnando la persona nel suo personale percorso di 
+            <span class="font-bold uppercase">guarigione</span>, congiuntamente, o meno, al lavoro di altri specialisti del settore sanitario che assolvono al compito della 
+            <span class="font-bold uppercase">cura</span>.
+        </p>
       </div>
     </div>
 </div>
---
+++ src/components/Who.svelte
@@ -0,0 +1,26 @@
+<script>
+    const description = [
+        'Paolo Sernini, classe 1998, è un professionista del settore olistico.', 
+        'Laureato in Scienze e Tecniche Psicologiche all’Università degli Studi di Torino con una tesi di analisi psicosociale su come il comportamento verso le persone affette da disabilità non intellettuale possa peggiorare la sintomatologia clinica della disabilità, decide poi di deviare il suo percorso di studi verso una concezione più larga di benessere. Si qualifica in Medicina Tradizionale Cinese preso il MEBO TCM Training Centre di Pachino e, poco dopo, in Sofrologia e Naturopatia, il tutto per poter ottenere una visione quanto più omnicomprensiva e omnisistemica del corpo umano, del suo funzionamento e delle sue ricadute psicopatologiche.',
+        'Appassionato di scrittura, si diletta fin dalla giovanissima età con diversi generi letterari, prediligendo comunque le tematiche di carattere psicologico e le elucubrazioni intellettuali. '
+    ];
+</script>
+
+<span id="bio"/>
+
+<div class="hero bg-[url(/bioBg.webp)] ">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content flex-col lg:flex-row">
+      <img src="/bioDetail.webp" class="mt-10 max-w-[10rem] sm:max-w-xs rounded-lg shadow-2xl" alt="logo"/>
+      <div class="max-w-4xl">
+        <h1 class="text-3xl lg:text-5xl font-bold pb-2 lg:pb-4">Chi è <span class="text-primary">Paolo Sernini</span> ?</h1>
+        <div class="text-md lg:w-5/6 text-justify">
+            {#each description as paragraph}
+                <p class="mb-1 lg:mb-5">
+                    {paragraph}
+                </p>
+            {/each}    
+      </div >
+      </div>
+    </div>
+</div>
