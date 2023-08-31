3174ac7933f80c6af51713e46759316855ca9c4f
ornato_t <tommy.ornato@gmail.com>
2023-08-31 19:48:47 +0200

    Fixing navbar, adding new page "utility"

---
+++ src/components/Utility.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  const points = [
+    {
+      bold: 'Problemi di stress',
+      normal: '',
+    }, {
+      bold: 'Cattivo stile di vita',
+      normal: '',
+    }, {
+      bold: 'Dolori e fastidi cronicizzati',
+      normal: 'nell\'ambito dell\'apparato muscolo-scheletrico',
+    }, {
+      bold: 'Problemi persistenti',
+      normal: 'non adeguatamente superati attraverso la medicina convenzionale',
+    }, {        
+      bold: 'Aiuto nel superamento di infiammazioni',
+      normal: 'muscolari cronicizzate, cervicalgie, lombosciatalgie',
+    }
+  ] satisfies point[];
+
+  interface point {
+    bold: string,
+    normal: string,
+  }
+</script>
+
+<span id="facilitatore"/>
+
+<div class="hero min-h-screen bg-[url(/therapy.webp)]">
+<!-- <div class="hero min-h-screen"> -->
+    <div class="hero-overlay bg-opacity-70"></div>
+    <div class="hero-content text-start text-neutral-content">
+      <div class="max-w-full lg:max-w-7xl">
+        <h1 class="mb-2 lg:mb-4 mt-6 px-4 lg:px-0 text-3xl lg:text-5xl font-bold text-center"> 
+          In cosa può esserti utile un 
+          <span class="text-secondary">facilitatore terapeutico</span>
+          ?
+        </h1>
+
+        <div class="join-vertical lg:my-8 my-4 lg:px-16 px-3">
+            {#each points as point}
+            <div class="list-disc pt-0.5">
+              <li>
+                <span class="font-bold">
+                  {point.bold}
+                </span>
+                  {point.normal}
+              </li>
+            </div>
+            {/each}
+        </div>
+
+        <div class="mb-5 text-justify lg:px-10">
+          <h2 class="font-semibold text-xl md:text-2xl">
+            Collocazione deontologica
+          </h2>
+          <p class="mt-0.5 text-sm md:text-base">
+            La professione del Facilitatore Terapeutico si rifà a quella del <span class="font-bold">Professionista in Tecniche Olistiche</span> e in quanto tale è esercitata ai sensi della legge <span class="italic">14 gennaio 2013 n°4 (G.U.N.22 del 26-01-2013)</span>.
+          </p>
+          <p class="mt-1.5 text-sm md:text-base">
+            Le indicazioni in tema di salute sono sempre da porre al vaglio del proprio medico e le eventuali diagnosi enunciate non hanno valore clinico, finché non validate da Specialisti in ambito sanitario. I consigli di supporto e i trattamenti sono sempre da inserire all’interno di un più ampio percorso delineato dai propri medici curanti.
+          </p>
+        </div>
+      </div>
+    </div>
+</div>
--- src/components/Who.svelte
+++ src/components/Who.svelte
@@ -2,7 +2,7 @@
     const description = [
         'Paolo Sernini, classe 1998, è un professionista del settore olistico.', 
         'Laureato in Scienze e Tecniche Psicologiche all’Università degli Studi di Torino con una tesi di analisi psicosociale su come il comportamento verso le persone affette da disabilità non intellettuale possa peggiorare la sintomatologia clinica della disabilità, decide poi di deviare il suo percorso di studi verso una concezione più larga di benessere. Si qualifica in Medicina Tradizionale Cinese preso il MEBO TCM Training Centre di Pachino e, poco dopo, in Sofrologia e Naturopatia, il tutto per poter ottenere una visione quanto più omnicomprensiva e omnisistemica del corpo umano, del suo funzionamento e delle sue ricadute psicopatologiche.',
-        'Appassionato di scrittura, si diletta fin dalla giovanissima età con diversi generi letterari, prediligendo comunque le tematiche di carattere psicologico e le elucubrazioni intellettuali. '
+        // 'Appassionato di scrittura, si diletta fin dalla giovanissima età con diversi generi letterari, prediligendo comunque le tematiche di carattere psicologico e le elucubrazioni intellettuali. '
     ];
 </script>
 
@@ -16,7 +16,7 @@
         <h1 class="text-3xl lg:text-5xl font-bold pb-2 lg:pb-4">Chi è <span class="text-primary">Paolo Sernini</span> ?</h1>
         <div class="text-md lg:w-5/6 text-justify">
             {#each description as paragraph}
-                <p class="mb-1 lg:mb-5">
+                <p class="mb-3 lg:mb-5">
                     {paragraph}
                 </p>
             {/each}    
