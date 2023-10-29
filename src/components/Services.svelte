b704de6e00e3ab53554f0d4cccc3f1709b0604de
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-29 22:32:34 +0100

    Adding mobile layout for services page

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -6,7 +6,7 @@
 <div class="hero min-h-screen bg-gradient-to-b from-emerald-200 via-cyan-200 to-blue-300">
     <div class="hero-content text-start">
         <div class="max-w-full lg:max-w-6xl">
-            <h1 class="mb-6 lg:mb-8 mt-6 px-4 lg:px-12 text-2xl lg:text-5xl font-bold text-center">
+            <h1 class="mb-4 lg:mb-8 mt-4 lg:mt-6 px-4 lg:px-12 text-2xl lg:text-5xl font-bold text-center">
                 La cura si nasconde sempre dietro alla domanda
                 <span class="text-rose-500">giusta</span>
                 al momento
@@ -14,28 +14,47 @@
             </h1>
 
             <!-- Mobile (borders) -->
-            <!-- <div class="border-rose-400 border-2 join join-vertical rounded-xl mb-4 md:hidden">
-        {#each points[0]s as points[0], i}
-          <div class="collapse collapse-arrow join-item border-rose-400 border-b">
-            <input type="radio" name="accordion" aria-label="menu espandibile {i}" />
-            <div class="collapse-title lg:text-xl font-semibold py-2">
-              {points[0].bold}
-            </div>
-            <div class="collapse-content text-sm lg:text-base">
-              <p>{points[0].normal}</p>
-            </div>
-          </div>
-        {/each}
-      </div> -->
+            <ul class="mb-2 flex flex-col gap-y-4 md:hidden">
+                <div class="flex gap-4 flex-row justify-self-start">
+                    <div class="flex flex-col justify-between">
+                        <div>
+                            <span class="text-sm">
+                                <p class="my-1.5">Spesso la poca efficacia delle cure intraprese non è da attribuirsi agli specialisti, ma all'osservare il sintomo troppo da vicino.</p>
+                                <p class="my-1.5 text-lg">
+                                    Per questo la <strong class="font-bold">durata delle sedute</strong> non è fissa, ma soggiace alla corretta ricostruzione dell'<strong class="font-bold"> anamnesi</strong> e alle <strong class="font-bold">necessità del paziente</strong>.
+                                </p>
+                            </span>
+                        </div>
+                        <p class="italic text-sm my-auto">Nota: la prima seduta dura in media due ore</p>
+                    </div>
+                </div>
+                <div class="flex gap-4 flex-row-reverse justify-self-end">
+                    <div class="flex flex-col">
+                        <span class="text-sm">
+                            <p class="my-1.5">Un'eccessiva settorializzazione della visione clinica può talvolta limitare la valutazione della problematica.</p>
+                            <p class="my-1.5 text-lg">Per questo avere una conoscenza sia della <strong class="font-bold">psicologia</strong> umana che delle pratiche terapeutiche <strong class="font-bold">occidentali e orientali</strong>, può aprire alla vera radice del problema.</p>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="flex gap-4 flex-row justify-self-start">
+                    <div class="flex flex-col">
+                        <span class="text-sm">
+                            <p class="my-1.5">Le crescenti distanze fisiche e umane tra paziente e curante possono costituire un forte ostacolo alla guarigione.</p>
+                            <p class="my-1.5 text-lg">Per questo si effettuano <strong class="font-bold">visite a domicilio</strong>. Previa valutazione, si offrono trasferte su tutto il <strong class="font-bold">territorio nazionale</strong>.</p>
+                        </span>
+                    </div>
+                </div>
+            </ul>
 
             <!-- Desktop -->
-            <ul class="mb-8 pl-8 hidden md:flex md:flex-col gap-y-3">
+            <ul class="mb-8 pl-8 hidden md:flex flex-col gap-y-3">
                 <div class="flex gap-4 flex-row justify-self-start">
                     <img src="/interview.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
                     <div class="flex flex-col justify-between">
                         <div>
-                            <span class="text-sm lg:text-base">
+                            <span>
                                 <p class="my-1.5">Spesso la poca efficacia delle cure intraprese non è da attribuirsi agli specialisti, ma all'osservare il sintomo troppo da vicino.</p>
                                 <p class="my-1.5 text-lg">
                                     Per questo la <strong class="font-bold">durata delle sedute</strong> non è fissa, ma soggiace alla corretta ricostruzione dell'<strong class="font-bold"> anamnesi</strong> e alle <strong class="font-bold">necessità del paziente</strong>.
@@ -52,7 +71,7 @@
                     <img src="/navigation.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
                     <div class="flex flex-col">
-                        <span class="text-sm lg:text-base">
+                        <span>
                             <p class="my-1.5">Un'eccessiva settorializzazione della visione clinica può talvolta limitare la valutazione della problematica.</p>
                             <p class="my-1.5 text-lg">Per questo avere una conoscenza sia della <strong class="font-bold">psicologia</strong> umana che delle pratiche terapeutiche <strong class="font-bold">occidentali e orientali</strong>, può aprire alla vera radice del problema.</p>
                         </span>
@@ -65,7 +84,7 @@
                     <img src="/home.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
                     <div class="flex flex-col">
-                        <span class="text-sm lg:text-base">
+                        <span>
                             <p class="my-1.5">Le crescenti distanze fisiche e umane tra paziente e curante possono costituire un forte ostacolo alla guarigione.</p>
                             <p class="my-1.5 text-lg">Per questo si effettuano <strong class="font-bold">visite a domicilio</strong>. Previa valutazione, si offrono trasferte su tutto il <strong class="font-bold">territorio nazionale</strong>.</p>
                         </span>
