a02cc27bfd313fde9a28e773273cd695b11e1f1c
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-29 19:43:58 +0100

    Adding "services" page on desktop

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -6,15 +6,15 @@
 <div class="hero min-h-screen bg-gradient-to-b from-emerald-200 via-cyan-200 to-blue-300">
     <div class="hero-content text-start">
         <div class="max-w-full lg:max-w-6xl">
-            <h1 class="mb-2 lg:mb-4 mt-6 px-4 lg:px-0 text-2xl lg:text-5xl font-bold text-center bg-cyan-400 p-4 rounded-lg">
+            <h1 class="mb-6 lg:mb-8 mt-6 px-4 lg:px-12 text-2xl lg:text-5xl font-bold text-center">
                 La cura si nasconde sempre dietro alla domanda
-                <span class="text-rose-500 uppercase">giusta</span>
+                <span class="text-rose-500">giusta</span>
                 al momento
-                <span class="text-rose-500 uppercase">giusto</span>
+                <span class="text-rose-500">giusto</span>
             </h1>
 
             <!-- Mobile (borders) -->
-            <!-- <div class="border-rose-400 border-2 join join-vertical rounded-xl mt-2 mb-4 md:hidden">
+            <!-- <div class="border-rose-400 border-2 join join-vertical rounded-xl mb-4 md:hidden">
         {#each points[0]s as points[0], i}
           <div class="collapse collapse-arrow join-item border-rose-400 border-b">
             <input type="radio" name="accordion" aria-label="menu espandibile {i}" />
@@ -29,48 +29,51 @@
       </div> -->
 
             <!-- Desktop -->
-            <ul class="mt-2 mb-8 pl-8 hidden md:flex md:flex-col">
+            <ul class="mb-8 pl-8 hidden md:flex md:flex-col gap-y-3">
                 <div class="flex gap-4 flex-row justify-self-start">
-                    <img src="/interview.jpg" alt="" class="w-48 rounded-md" />
+                    <img src="/interview.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
-                    <div class="flex flex-col">
-                        <span class="text-sm lg:text-base">
-                            <p class="my-1.5">La poca efficacia delle cure intraprese spesso non è da attribuirsi agli specialisti, ma all'osservare il sintomo troppo da vicino.</p>
-                            <p class="my-1.5">Per questo la durata delle sedute non è stimata in base al tempo, ma soggiace alla corretta ricostruzione dell'anamnesi e alle necessità del paziente.</p>
-                        </span>
-                        <p class="italic text-sm">Nota: la prima seduta dura in media due ore</p>
+                    <div class="flex flex-col justify-between">
+                        <div>
+                            <span class="text-sm lg:text-base">
+                                <p class="my-1.5">Spesso la poca efficacia delle cure intraprese non è da attribuirsi agli specialisti, ma all'osservare il sintomo troppo da vicino.</p>
+                                <p class="my-1.5 text-lg">
+                                    Per questo la <strong class="font-bold">durata delle sedute</strong> non è fissa, ma soggiace alla corretta ricostruzione dell'<strong class="font-bold"> anamnesi</strong> e alle <strong class="font-bold">necessità del paziente</strong>.
+                                </p>
+                            </span>
+                        </div>
+                        <p class="italic text-sm my-auto">Nota: la prima seduta dura in media due ore</p>
                     </div>
+
+                    <div class="w-[19rem]" />
                 </div>
 
                 <div class="flex gap-4 flex-row-reverse justify-self-end">
-                    <img src="/navigation.jpg" alt="" class="w-48 rounded-md" />
+                    <img src="/navigation.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
                     <div class="flex flex-col">
                         <span class="text-sm lg:text-base">
-                            <p class="my-1.5">A volte l'eccessiva settorializzazione della visione limita la possibilità di valutare la problematica nell'interezza della sua manifestazione.</p>
-                            <p class="my-1.5">Per questo avere una conoscenza sia della psicologia umana che delle pratiche terapeutiche occidentali e orientali, può aprire alla vera radice del problema.</p>
+                            <p class="my-1.5">Un'eccessiva settorializzazione della visione clinica può talvolta limitare la valutazione della problematica.</p>
+                            <p class="my-1.5 text-lg">Per questo avere una conoscenza sia della <strong class="font-bold">psicologia</strong> umana che delle pratiche terapeutiche <strong class="font-bold">occidentali e orientali</strong>, può aprire alla vera radice del problema.</p>
                         </span>
                     </div>
+
+                    <div class="w-[19rem]" />
                 </div>
 
                 <div class="flex gap-4 flex-row justify-self-start">
-                    <img src="/home.jpg" alt="" class="w-48 rounded-md" />
+                    <img src="/home.jpg" alt="" class="w-48 h-48 rounded-xl" />
 
                     <div class="flex flex-col">
                         <span class="text-sm lg:text-base">
-                            <p class="my-1.5">La sempre maggiore distanza in termini sia fisici che umani tra paziente e curante può essere fortemente d'ostacolo alla guarigione.</p>
-                            <p class="my-1.5">Per questo si effettuano visite a domicilio. Previa valutazione, si offrono trasferte su tutto il territorio nazionale.</p>
+                            <p class="my-1.5">Le crescenti distanze fisiche e umane tra paziente e curante possono costituire un forte ostacolo alla guarigione.</p>
+                            <p class="my-1.5 text-lg">Per questo si effettuano <strong class="font-bold">visite a domicilio</strong>. Previa valutazione, si offrono trasferte su tutto il <strong class="font-bold">territorio nazionale</strong>.</p>
                         </span>
                     </div>
+
+                    <div class="w-[19rem]" />
                 </div>
             </ul>
-
-            <!-- <p class="mb-5 text-justify">
-        Il Facilitatore Terapeutico, quindi, va ad assolvere al ruolo del
-        <span class="italic">"maestro"</span>, istruendo, correggendo e accompagnando la persona nel suo personale percorso di
-        <span class="font-bold">guarigione</span>, congiuntamente, o meno, al lavoro di altri specialisti del settore sanitario che assolvono al compito della
-        <span class="font-bold">cura</span>.
-      </p> -->
         </div>
     </div>
 </div>
